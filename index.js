d36f29f315bcc5c55ceeb353a6b52743367bb5db
Hanna-Mari Elo <hannamari.elo@outlook.com>
2023-12-12 00:07:21 +0200

    display single phonebook entry

--- index.js
+++ index.js
@@ -32,8 +32,18 @@ app.get('/api/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
   res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 const PORT = 3001
