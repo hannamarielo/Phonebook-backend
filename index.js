1b5268a607e8135c9aaa8fff472757390d63d1bc
Hanna-Mari Elo <hannamari.elo@outlook.com>
2023-12-12 11:39:09 +0200

    configuring morgan to log to console

--- index.js
+++ index.js
@@ -1,94 +1,99 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/info', (req, res) => {
-  const time = new Date()
-  res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${time}</p>`)
-})
+app.get("/api/info", (req, res) => {
+  const time = new Date();
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p> <p>${time}</p>`
+  );
+});
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find((person) => person.id === id)
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
 
   if (person) {
-    res.json(person)
+    res.json(person);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-})
+});
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
 
-  res.status(204).end()
-})
-
+  res.status(204).end();
+});
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(person => person .id))
-    : 0
-  return maxId + 1
-}
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-  const nameExists= persons.find((person) => person.name.toLowerCase() === body.name.toLowerCase())
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  const nameExists = persons.find(
+    (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  );
 
   if (!body.name || !body.number) {
-    return res.status(400).json({ 
-      error: 'invalid name or number' 
-    })
-
-  } if (nameExists) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
+    return res.status(400).json({
+      error: "invalid name or number",
+    });
+  }
+  if (nameExists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
   }
 
   const person = {
     id: generateId(),
     name: body.name,
     number: body.number,
-  }
+  };
 
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  res.json(person)
-})
+  res.json(person);
+});
 
-const PORT = 3001
+const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
